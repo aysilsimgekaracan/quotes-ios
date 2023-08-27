d790f47f38cf437495194857f3126769b2c27add
Ayşıl Simge Karacan <aysilsimge@gmail.com>
2023-08-27 10:19:04 +0300

    Initial Commit

---
+++ quotes/quotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  quotesApp.swift
+//  quotes
+//
+//  Created by Ayşıl Simge Karacan on 27.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct quotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
